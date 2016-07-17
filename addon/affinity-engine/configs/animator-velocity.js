997de45ad2f5d2841b198509c6811d38fec3ee6d
null null null <null@affinitygamescollective.org>
2016-07-17 11:24:05 -0700

    add configs

--- addon/affinity-engine/configs/animator-velocity.js
+++ addon/affinity-engine/configs/animator-velocity.js
@@ -1,5 +1,25 @@
 export default {
   priority: 1,
+  transition: {
+    duration: 250,
+    effect: { opacity: 1 }
+  },
+  transitionDuration: 200,
+  transitionIn: {
+    duration: 250,
+    effect: { opacity: [1, 0] }
+  },
+  transitionOut: {
+    duration: 250,
+    effect: { opacity: 0 }
+  },
+  tweenEffect: {
+    opacity: {
+      from: 0,
+      to: 1
+    }
+  },
+  tweenRate: 25,
   'affinity-engine': {
     'animator': {
       path: 'service:affinity-engine/animator-velocity',
