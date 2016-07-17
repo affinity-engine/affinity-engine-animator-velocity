4d1caa9410f9df4921fe2262886cce26748c1686
null null null <null@affinitygamescollective.org>
2016-07-17 12:55:39 -0700

    place inside globals

--- addon/affinity-engine/configs/animator-velocity.js
+++ addon/affinity-engine/configs/animator-velocity.js
@@ -1,25 +1,27 @@
 export default {
   priority: 1,
-  transition: {
-    duration: 250,
-    effect: { opacity: 1 }
+  globals: {
+    transition: {
+      duration: 250,
+      effect: { opacity: 1 }
+    },
+    transitionDuration: 200,
+    transitionIn: {
+      duration: 250,
+      effect: { opacity: [1, 0] }
+    },
+    transitionOut: {
+      duration: 250,
+      effect: { opacity: 0 }
+    },
+    tweenEffect: {
+      opacity: {
+        from: 0,
+        to: 1
+      }
+    },
+    tweenRate: 25
   },
-  transitionDuration: 200,
-  transitionIn: {
-    duration: 250,
-    effect: { opacity: [1, 0] }
-  },
-  transitionOut: {
-    duration: 250,
-    effect: { opacity: 0 }
-  },
-  tweenEffect: {
-    opacity: {
-      from: 0,
-      to: 1
-    }
-  },
-  tweenRate: 25,
   'affinity-engine': {
     'animator': {
       path: 'service:affinity-engine/animator-velocity',
