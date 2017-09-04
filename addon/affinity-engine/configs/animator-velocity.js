23d75a4658118d22975b5244d275afa3831541ff
null null null <null@affinitygamescollective.org>
2017-09-04 15:28:57 -0400

    update deps

--- addon/affinity-engine/configs/animator-velocity.js
+++ addon/affinity-engine/configs/animator-velocity.js
@@ -1,25 +1,27 @@
 export default {
   priority: 2,
   global: {
-    animationLibrary: 'velocity',
-    transition: {
-      duration: 250,
-      effect: { opacity: 1 }
-    },
-    transitionDuration: 200,
-    transitionIn: {
-      duration: 250,
-      effect: { opacity: [1, 0] }
-    },
-    transitionOut: {
-      duration: 250,
-      effect: { opacity: 0 }
-    },
-    lxlTransition: {
-      effect: {
-        opacity: [1, 0]
+    attrs: {
+      animator: 'velocity',
+      transition: {
+        duration: 250,
+        effect: { opacity: 1 }
       },
-      rate: 25
+      transitionIn: {
+        duration: 250,
+        effect: { opacity: [1, 0] }
+      },
+      transitionOut: {
+        duration: 250,
+        effect: { opacity: 0 }
+      },
+      lxlAnimation: {
+        effect: {
+          opacity: [1, 0]
+        },
+        duration: 0,
+        rate: 25
+      }
     }
   },
   registrant: {
